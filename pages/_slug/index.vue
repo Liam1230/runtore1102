f29f22d1425a2823f2cf5e3121dc4e934ea982ef
Liam1230 <kinoko.sd@gmail.com>
2020-11-02 15:09:59 +0900

    first

---
+++ pages/_slug/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container column justify-center>
+    <h1 class="title">{{ title }}</h1>
+    <!-- <p class="publishedAt">{{ publishedAt }}</p> -->
+    <div class="post" v-html="body"></div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData(ctx) {
+    const { data } = await ctx.$axios.get(
+      `https://valuup.microcms.io/api/v1/blog/${ctx.params.slug}`,
+      {
+        headers: { 'X-API-KEY': '66a46bb5-4cfd-4a48-903d-cd75f0ad4cd6' }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container column justify-center>
+    <ul>
+      <li v-for="content in contents" :key="content.id">
+        <nuxt-link :to="`/${content.id}`">
+          {{ content.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData(ctx) {
+    const { data } = await ctx.$axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      `https://valuup.microcms.io/api/v1/blog`,
+      {
+        headers: { 'X-API-KEY': '66a46bb5-4cfd-4a48-903d-cd75f0ad4cd6' }
+      }
+    )
+    return data
+  }
+}
+</script>
