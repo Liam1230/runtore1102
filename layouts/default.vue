5d71ae2c2f1726871be7cceb9643b63ad30e50e1
Liam1230 <kinoko.sd@gmail.com>
2020-11-02 23:28:14 +0900

    トップぺージ実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,20 +3,42 @@
     <v-app-bar :clipped-left="clipped" fixed color="rgba(0, 173, 255, 0.7)">
       <v-toolbar-title class="ml-3 white--text" v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <nuxt-link :to="item.to" v-for="(item,i) in items" :key="i" class="white--text mx-5">
+        {{item.title}}
+      </nuxt-link>
     </v-app-bar>
     <v-main>
       <transition name="fade-transform">
         <nuxt />
       </transition>
     </v-main>    
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer class="white--text py-5" color="#00ADFF">
+      <v-container>
+        <v-row class="justify-center align-center">
+          <v-col cols="5" class="text-center">
+            <span>&copy; {{ new Date().getFullYear() }} 大人のRunトレ塾</span>
+          </v-col>
+          <v-col cols="5" class="pl-5 py-0" style="border-left: solid 2px #ffffff;">
+            <table style="border-spacing: 30px 10px;">
+              <tr>
+                <td>TEL</td>
+                <td>000-0000-0000</td>
+              </tr>
+              <tr>
+                <td>EMAIL</td>
+                <td>aaaaa@xxx.jp</td>
+              </tr>
+              <tr>
+                <td>ADRESS</td>
+                <td>
+                  〒000-0000<br>
+                  大阪府大阪市・・・・・
+                </td>
+              </tr>
+            </table>
+          </v-col>
+        </v-row>        
+      </v-container>
     </v-footer>
   </v-app>
 </template>
@@ -30,15 +52,21 @@ export default {
       fixed: true,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          title: '月間100kmメソッド',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          title: '痛み',
           to: '/inspire'
-        }
+        },
+        {
+          title: 'フォーム',
+          to: '/inspire'
+        },
+        {
+          title: 'LINE練習帳',
+          to: '/inspire'
+        },
       ],
       miniVariant: false,
       right: true,
--- pages/index.vue
+++ pages/index.vue
@@ -14,23 +14,23 @@
     </section>
     <section class="my-5 py-5">
       <v-container column justify-center class="text-center">
-        <h1 class="text-h3 mt-5">Runトレについて</h1>
-        <h2 class="mt-3">-About-</h2>
-        <h3 class="text-h4 my-5 blue-sub-title">一人ではたどりつけない自己ベストへ</h3>
-        <p class="text-h6 text-left py-5">ランニングの痛みや ランニングフォームで悩んでいるあなたへ！「たくさん練習をこなしてもベストが更新できない」「怪我を繰り返す」 本当につらいものですよね 。多くのランナーが同じ悩みでつまずいてしまっています。あなた１人で背負い込む必要はありません！ この情報サイト の中で 、他の多くのランナーの悩みとともに 改善策を提示していきます。 その中にあなたの悩みを改善するヒントがあるかもしれません！</p>
+        <h1 class="text-h3 mt-5 title-text-color">Runトレについて</h1>
+        <h2 class="mt-3 title-text-color">-About-</h2>
+        <h3 class="text-h4 my-5 py-5 blue-sub-title">一人ではたどりつけない自己ベストへ</h3>
+        <p class="text-h6 text-left py-5 main-text-color">ランニングの痛みや ランニングフォームで悩んでいるあなたへ！「たくさん練習をこなしてもベストが更新できない」「怪我を繰り返す」 本当につらいものですよね 。多くのランナーが同じ悩みでつまずいてしまっています。あなた１人で背負い込む必要はありません！ この情報サイト の中で 、他の多くのランナーの悩みとともに 改善策を提示していきます。 その中にあなたの悩みを改善するヒントがあるかもしれません！</p>
       </v-container>
     </section>
     <section class="my-5 py-5">
       <v-container column justify-center class="text-center">
-        <v-row class="align-center">
-          <v-col cols=6>
+        <v-row class="align-center justify-space-between">
+          <v-col cols=5 class="mr-5">
             <v-img src="/img/profile.png"></v-img>
           </v-col>
-          <v-col cols=6 class="text-center">
-            <h1 class="text-h3 text-left main-color">大人のRunトレ塾</h1>
-            <h2 class="mt-3 text-left">代表 吉野洸希</h2>
+          <v-col cols=6 class="pl-5 text-center">
+            <h1 class="text-h4 text-left main-color">大人のRunトレ塾</h1>
+            <h2 class="text-h6 mt-3 text-left title-text-color">代表 吉野洸希</h2>
             <v-divider class="my-5"></v-divider>
-            <p class="text-left">私は月間走行距離100ｋｍで 2:48:56 を 実現しました。初 のフルマラソンで は月間 250 ｋｍ練習し、 膝の痛みや 足底筋膜炎に顔をゆがめながら も 3 45で ゴール。 もっと良いタイムが出ると思っていたのに、 走っても ベストは伸びないばかりか、 痛みが出て しまう。ただ走るだけの 非効率な練習に疑問 を持つように 。そこで 当サイトでお伝えして いる 月間 100 ｋｍメソッドを生み出しました！</p>
+            <p class="text-left main-text-color">私は月間走行距離100ｋｍで 2:48:56 を 実現しました。初 のフルマラソンで は月間 250 ｋｍ練習し、 膝の痛みや 足底筋膜炎に顔をゆがめながら も 3 45で ゴール。 もっと良いタイムが出ると思っていたのに、 走っても ベストは伸びないばかりか、 痛みが出て しまう。ただ走るだけの 非効率な練習に疑問 を持つように 。そこで 当サイトでお伝えして いる 月間 100 ｋｍメソッドを生み出しました！</p>
             <v-btn color="primary" large rounded>詳しく見る</v-btn>
           </v-col>
         </v-row>
@@ -39,11 +39,11 @@
     <section class="my-5 pb-5 p-relative">
       <img class="p-absolute absolute-lt" style="top:10%;" width="90%" src="/img/sectiontop1.png">
       <img style="width:100vw;" src="/img/blueTriangle.png">
-      <div class="main-color-bg pt-5" style="padding-bottom:120px;">
+      <div class="main-color-bg" style="padding-bottom:120px; padding-top:70px;">
         <v-container column justify-center class="text-center main-color-bg" style="margin-top:-1px;">
           <h1 class="text-h3 mt-5 pt-5 white--text">フォームの重要性について</h1>
           <h2 class="mt-3 white--text">-Form-</h2>
-          <h3 class="text-h4 my-5 yellow-sub-title">独自の型で無理に走り続けていませんか？</h3>
+          <h3 class="text-h4 my-5 py-5 yellow-sub-title">独自の型で無理に走り続けていませんか？</h3>
           <p class="text-h6 text-left py-5 white--text">練習量をこなしてもタイムが伸びない。多くのランナーの悩みの種です。それは 体力に問題があるのではなく 、ロスの多いランニングフォーム のせいかもしれません。ランニングは手軽に始めやすいために自己流のフォームになってしまいがち。 最初はそれでも良いですが、高みを目指すなら確実に頭打ちが来てしまいます。どんどん進む楽なフォームを身に付け、周りを魅了する格好いいフォームを身につけましょう！</p>
           <v-btn color="secondary" rounded x-large class="mb-5">無料でランニング力をチェック</v-btn>
         </v-container>
@@ -53,15 +53,65 @@
       <v-img width="90%" style="margin-left: auto;" src="/img/sectiontop2.png"></v-img>
       <div class="">
         <v-container column justify-center class="text-center" style="margin-top:-1px;">
-          <h1 class="text-h3 mt-5 pt-5">痛みをそのまま放置しないために</h1>
-          <h2 class="mt-3">-Pain-</h2>
-          <h3 class="text-h4 my-5 blue-sub-title">我慢して痛みを放置しすぎると…</h3>
-          <p class="text-h6 text-left py-5">ランニング中の痛みは本当に辛いもの。故障・痛みを治すために練習を休めば体力は落ちるし、ただ、痛みを放置して練習を続けると、どんどん体はボロボロになり、走れない体になってしまいます「怪我が癖になってしまっている」「ランニングをしていれば必ずどこかに痛みは出るもの」それは痛みの原因を改善することで変えることが可能。痛みは誤った体の使い方であることを知らせる合図。 正しい知識があれば、怪我は更なるレベルアップの 大きな チャンスなのです！その方法をお伝えします。</p>
+          <h1 class="text-h3 mt-5 pt-5 title-text-color">痛みをそのまま放置しないために</h1>
+          <h2 class="mt-3 title-text-color">-Pain-</h2>
+          <h3 class="text-h4 my-5 py-5 blue-sub-title">我慢して痛みを放置しすぎると…</h3>
+          <p class="text-h6 text-left py-5 main-text-color">ランニング中の痛みは本当に辛いもの。故障・痛みを治すために練習を休めば体力は落ちるし、ただ、痛みを放置して練習を続けると、どんどん体はボロボロになり、走れない体になってしまいます「怪我が癖になってしまっている」「ランニングをしていれば必ずどこかに痛みは出るもの」それは痛みの原因を改善することで変えることが可能。痛みは誤った体の使い方であることを知らせる合図。 正しい知識があれば、怪我は更なるレベルアップの 大きな チャンスなのです！その方法をお伝えします。</p>
         </v-container>
       </div>
     </section>
     <section class="my-5 py-5 p-relative text-center">
-      <v-btn x-large color="primary" style="padding:2.5rem; padding-left:4rem; padding-right:4rem;">あなたが気になる<br>悩みはどっち？</v-btn>
+      <p class="main-color-bg pa-5 white--text text-h5" style="display: inline-block;">あなたが気になる悩みはどっち？</p>
+      <v-row class="justify-center align-start pt-5 mt-5">
+        <v-col cols="5">
+          <v-btn rounded x-large @click="formClick" color="secondary">フォーム</v-btn>
+          <v-expansion-panels flat v-model="formOpen" multiple class="mt-5">
+            <v-expansion-panel v-for="(item,i) in 1" :key="i">
+              <v-expansion-panel-content>
+                <v-card class="pa-3 mx-0 my-3" color="#FFE200" v-for="(content, i) in contents" :key="i">
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title class="headline" v-text="content.title"></v-card-title>
+                    </div>
+                    <v-avatar>
+                      <v-img :src="content.header.url"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+        <span class="text-h3 mx-5 title-text-color">or</span>
+        <v-col cols="5">
+          <v-btn rounded x-large @click="painClick" color="secondary">痛み</v-btn>
+          <v-expansion-panels flat v-model="painOpen" multiple class="mt-5">
+            <v-expansion-panel v-for="(item,i) in 1" :key="i">
+              <v-expansion-panel-content>
+                <v-card class="py-3 my-3" color="#FFE200" v-for="(content, i) in contents" :key="i">
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title class="headline" v-text="content.title"></v-card-title>
+                    </div>
+                    <v-avatar>
+                      <v-img :src="content.header.url"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </section>
+    <hr style="">
+    <section class="py-5 my-5">
+      <v-container class="text-center">
+        <h3 class="text-h4 my-5 blue-sub-title">無料でランニング力をチェック</h3>
+        <p class="text-h6 text-left py-5 main-text-color">スポーツトレーナーとして多くのランナーの怪我やランニングフォームの相談を受けてきました。経験をもとに解決策を当サイトにまとめております。痛み・ランニングフォームのあなたのお悩みに合った記事をご覧ください！</p>
+        <v-btn color="secondary" rounded x-large class="my-5">無料でランニング力をチェック</v-btn>
+        <p class="text-h6 text-left py-5 mt-5 main-text-color">あなたのランニング力チェックしませんか？ランナーに必要だと考える「ランニング技術」「練習内容」「体に関する知識」の3項目から問題を作成しております。5分で終了する直感で選ぶ問題で、あなたの強みや弱点を発見しましょう！その弱点に合わせ、あなたにオススメの記事も用意しております！</p>
+      </v-container>
     </section>
   </article>
   <!-- <v-container column justify-center> -->
@@ -78,6 +128,26 @@
 
 <script>
 export default {
+  data:()=>({
+    formOpen:[],
+    painOpen:[]
+  }),
+  methods:{
+    formClick(){
+      if(this.formOpen.length){
+        this.formOpen = []
+      }else{
+        this.formOpen = [0]
+      }
+    },
+    painClick(){
+      if(this.painOpen.length){
+        this.painOpen = []
+      }else{
+        this.painOpen = [0]
+      }
+    }
+  },
   async asyncData(ctx) {
     const { data } = await ctx.$axios.get(
       // your-service-id部分は自分のサービスidに置き換えてください
