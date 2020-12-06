fe01093fe4c89fa5b456c5541e7d2a899a48d401
Liam1230 <Kin0k0daichi>
2020-12-06 15:20:57 +0900

    画像サイズの固定化

--- pages/index.vue
+++ pages/index.vue
@@ -76,7 +76,7 @@
           <v-expansion-panels flat v-model="formOpen" multiple class="mt-5">
             <v-expansion-panel class="">
               <v-expansion-panel-content>
-                <v-card height="100px" class="my-3" color="#FFE200" v-for="(content, i) in formcontens" :key="i">
+                <v-card class="my-3 expantion-content" color="#FFE200" v-for="(content, i) in formcontens" :key="i">
                   <nuxt-link :to="`/blogCategory?categoryId=${content.id}`">
                     <v-row class="d-flex black--text flex-no-wrap justify-space-between py-0">
                       <v-col cols="8" class="d-flex align-center pt-2 pl-5">
@@ -89,8 +89,8 @@
                         <!-- <v-card-title class="text-h5" v-text=""></v-card-title> -->
                       </v-col>
                       <v-col cols="4" class="py-0">
-                        <v-img v-if="content.image" height="100px" :src="content.image.url"></v-img>
-                        <v-img v-else height="100px" src="/img/Run_tore_rogo.png"></v-img>
+                        <v-img v-if="content.image" :height="$vuetify.breakpoint.mdAndUp ? '120px' : '60px' " :src="content.image.url"></v-img>
+                        <v-img v-else  :height="$vuetify.breakpoint.mdAndUp ? '120px' : '60px'" src="/img/Run_tore_rogo.png"></v-img>
                       </v-col>
                     </v-row>
                   </nuxt-link>
@@ -107,7 +107,7 @@
           <v-expansion-panels flat v-model="painOpen" multiple class="mt-5 px-0">
             <v-expansion-panel>
               <v-expansion-panel-content>
-                <v-card height="100px" class="my-3" color="#FFE200" v-for="(content, i) in paincontens" :key="i">
+                <v-card class="my-3 expantion-content" color="#FFE200" v-for="(content, i) in paincontens" :key="i">
                   <nuxt-link :to="`/blogCategory?categoryId=${content.id}`">
                     <v-row class="d-flex black--text flex-no-wrap justify-space-between pa-0">
                       <v-col cols="8" class="d-flex align-center pt-2 pl-5">
@@ -120,8 +120,8 @@
                         <!-- <v-card-title class="text-h5" v-text=""></v-card-title> -->
                       </v-col>
                       <v-col cols="4" class="py-0">
-                        <v-img v-if="content.image" height="100px" :src="content.image.url"></v-img>
-                        <v-img v-else height="100px" src="/img/Run_tore_rogo.png"></v-img>
+                        <v-img v-if="content.image" :height="$vuetify.breakpoint.mdAndUp ? '120px' : '60px' " :src="content.image.url"></v-img>
+                        <v-img v-else  :height="$vuetify.breakpoint.mdAndUp ? '120px' : '60px'" src="/img/Run_tore_rogo.png"></v-img>
                       </v-col>
                     </v-row>
                   </nuxt-link>
